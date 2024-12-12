d32e6a51a59c2aace56f3b9c64f4e83deb699001
AMRITHAVARSHNI19 <amusk192k5@gmail.com>
2024-12-12 11:55:41 +0530

    Initial commit

---
+++ src/main/java/com/infosys/carRentalSystem/CarRentalSystemApplication.java
@@ -0,0 +1,21 @@
+package com.infosys.carRentalSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSystemApplication.class, args);
+	}
+
+}
+//id:Amritha,password:1234567,Role:Customer
+//id:Varshni,password:7654321,Role:Admin
+//id:Lotus,password:Mango,Role:Admin
+//id:Tulip,password:Apple,Role:Customer
+//id:Sunflower,password:flower,Role:Customer
+//id:Lily,password:lilyflower,Role:Customer
+//Rose-sorry renew
+//Sunflower-payment(mainn)
